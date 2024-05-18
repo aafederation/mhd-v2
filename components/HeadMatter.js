9fd82004326bbb1d44386f3634f33db71f8255c4
rushilshakya <rushil.shakya@gmail.com>
2024-05-18 15:36:18 +0545

    feat: headmatter complete

---
+++ components/HeadMatter.js
@@ -0,0 +1,60 @@
+import Head from "next/head";
+
+export const HeadMatter = () => {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+
+      {/* facebook open graph tag */}
+      <meta property="og:url" content="https://mhd.aafederation.org/" />
+      <meta property="og:type" content="article" />
+      <meta
+        property="og:title"
+        content="Asian American Federation Mental Health Directory"
+      />
+      <meta
+        property="og:description"
+        content="Asian American Federation Mental Health Directory"
+      />
+      <meta property="og:image" content="/img/logo/AAF-Logo.svg" />
+      {/* facebook open graph tag */}
+
+      {/* twitter card  */}
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@AAFederation" />
+      <meta
+        name="twitter:title"
+        content="Asian American Federation Mental Health Directory"
+      />
+      <meta
+        name="twitter:description"
+        content="Asian American Federation Mental Health Directory"
+      />
+      <meta
+        name="twitter:image:src"
+        content="https://mhd.aafederation.org//img/logo/AAF-Logo.svg"
+      />
+      {/* twitter card */}
+
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>TEJ</title>
+      <link
+        rel="shortcut icon"
+        type="image/png"
+        href="/images/logos/fav-icon_1.png"
+      />
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <link rel="stylesheet" href="/css/global.css" />
+      <link rel="stylesheet" href="/fonts/fonts.css" />
+      <title>AAF Mental Health Directory</title>
+      <link
+        rel="shortcut icon"
+        type="image/png"
+        href="/img/icon/aaf-favicon.png"
+      />
+    </Head>
+  );
+};
--- components/Layout.js
+++ components/Layout.js
@@ -1,28 +1,33 @@
-import Link from 'next/link'
-import Head from 'next/head'
+import { HeadMatter } from "./HeadMatter";
 
 export const Layout = (props) => {
   return (
-    <div
-      style={{
-        margin: '3rem',
-      }}
-    >
-      <Head>
-        <title>Tina App</title>
-        <meta name="description" content="A TinaCMS Application" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        {' | '}
-        <Link href="/posts">
-          <a>Posts</a>
-        </Link>
-      </header>
-      <main>{props.children}</main>
-    </div>
-  )
-}
+    // <div
+    //   style={{
+    //     margin: "3rem",
+    //   }}
+    // >
+    //   <Head>
+    //     <title>Tina App</title>
+    //     <meta name="description" content="A TinaCMS Application" />
+    //     <link rel="icon" href="/favicon.ico" />
+    //   </Head>
+    //   <header>
+    //     <Link href="/">
+    //       <a>Home</a>
+    //     </Link>
+    //     {" | "}
+    //     <Link href="/posts">
+    //       <a>Posts</a>
+    //     </Link>
+    //   </header>
+    //   <main>{props.children}</main>
+    // </div>
+    <>
+      <HeadMatter />
+      <Menu />
+      {children}
+      <Footer />
+    </>
+  );
+};
