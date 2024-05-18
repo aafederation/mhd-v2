88fdda7ce7a43eedada540e9199ad62325907c0e
rushilshakya <rushil.shakya@gmail.com>
2024-05-18 16:15:21 +0545

    fix: syntax

--- components/Layout.js
+++ components/Layout.js
@@ -1,4 +1,7 @@
 import { HeadMatter } from "./HeadMatter";
+import { TopBar } from "./TopBar";
+import { Menu } from "./Menu";
+import { Footer } from "./Footer";
 
 export const Layout = ({ children }) => {
   return (
