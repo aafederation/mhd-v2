426acab963ba99119720b1bccdc486e425afda76
rushilshakya <rushil.shakya@gmail.com>
2024-05-20 22:51:56 +0545

    refactor: take out commented code

--- components/Layout.js
+++ components/Layout.js
@@ -5,27 +5,6 @@ import { Footer } from "./Footer";
 
 export const Layout = ({ children }) => {
   return (
-    // <div
-    //   style={{
-    //     margin: "3rem",
-    //   }}
-    // >
-    //   <Head>
-    //     <title>Tina App</title>
-    //     <meta name="description" content="A TinaCMS Application" />
-    //     <link rel="icon" href="/favicon.ico" />
-    //   </Head>
-    //   <header>
-    //     <Link href="/">
-    //       <a>Home</a>
-    //     </Link>
-    //     {" | "}
-    //     <Link href="/posts">
-    //       <a>Posts</a>
-    //     </Link>
-    //   </header>
-    //   <main>{props.children}</main>
-    // </div>
     <>
       <HeadMatter />
       <TopBar />
