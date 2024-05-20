b5074d83dab26e96a627b10469ba020246920113
rushilshakya <rushil.shakya@gmail.com>
2024-05-20 22:02:14 +0545

    feat: single page data working

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,38 +1,32 @@
-import { TinaMarkdown } from "tinacms/dist/rich-text";
 import { Layout } from "../components/Layout";
-import { tinaField, useTina } from "tinacms/dist/react";
-import { client } from "../tina/__generated__/client";
+import { getAllProviderIds, getSingleProviderFromSlug } from "../lib/getData";
 
-export default function Home(props) {
-  // data passes though in production mode and data is updated to the sidebar data in edit-mode
-  const { data } = useTina({
-    query: props.query,
-    variables: props.variables,
-    data: props.data,
-  });
+const memberType = "MHD";
 
-  const content = data.page.body;
+export default function MHDprovider({ mentalHealthProvider }) {
   return (
     <Layout>
-      <div data-tina-field={tinaField(data.page, "body")}>
-        <TinaMarkdown content={content} />
-      </div>
+      <h1>The org is {mentalHealthProvider.org}</h1>
     </Layout>
   );
 }
 
-// This is an example of a page generated with Serverside Rendering.
-// This can be switched to a static page by using getStaticProps
-export const getServerSideProps = async ({ params }) => {
-  const { data, query, variables } = await client.queries.page({
-    relativePath: `${params.slug}.mdx`,
-  });
+export async function getStaticPaths() {
+  const paths = getAllProviderIds(memberType);
+  return {
+    paths,
+    fallback: false,
+  };
+}
 
+export async function getStaticProps({ params }) {
+  const mentalHealthProvider = getSingleProviderFromSlug(
+    params.slug,
+    memberType
+  );
   return {
     props: {
-      data,
-      query,
-      variables,
+      mentalHealthProvider,
     },
   };
-};
+}
