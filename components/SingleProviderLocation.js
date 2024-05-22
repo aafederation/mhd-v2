9de4de510b2d54d25596bdad09c4976ae62df511
rushilshakya <rushil.shakya@gmail.com>
2024-05-22 14:22:44 +0545

    feat: complete bottom of the single provider page

--- components/SingleProviderLocation.js
+++ components/SingleProviderLocation.js
@@ -14,82 +14,127 @@ export const SingleProviderLocation = ({ provider, location }) => {
               items={location.credentials}
             />
           )}
-
-          {/* {{ with $location.trainings }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.trainings") "items" .) }}
-    				{{ end }}
-        */}
+          {location.trainings && (
+            <SingleServiceList title="Trainings" items={location.trainings} />
+          )}
           {location.services.length > 0 && (
             <SingleServiceList
               title="Clinical Services"
               items={location.services}
             />
           )}
-
-          {/*
-    				{{ with $location.psychotherapy }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.psychotherapy_specialties") "items" $location.psychotherapy_specialties) }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.psychotherapy_types") "items" $location.psychotherapy_types) }}
-    				{{ end }}
-        */}
+          {location.psychotherapy > 0 && (
+            <>
+              <SingleServiceList
+                title="PSYCHOTHERAPY SPECIALTIES"
+                items={location.psychotherapy_specialties}
+              />
+              <SingleServiceList
+                title="PSYCHOTHERAPY TYPES"
+                items={location.psychotherapy_types}
+              />
+            </>
+          )}
           {provider.payment_types.length > 0 && (
             <SingleServiceList
               title="PAYMENT TYPES (CLINICAL)"
               items={provider.payment_types}
             />
           )}
-          {/*
-    				{{ with $org.payment_info_clinical }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "payment_info_clinical") "items" .) }}
-    				{{ end }}
-    				{{ with $org.sliding_scale_clinical }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "sliding_scale_clinical") "items" .) }}
-    				{{ end }}
-    				{{ with $location.non_clinical_services }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.non_clinical_services") "items" .) }}
-    				{{ end }}
-    				{{ with $org.payment_types_non_clinical }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "payment_types_non_clinical") "items" .) }}
-    				{{ end }}
-    				{{ with $org.payment_info_non_clinical }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "payment_info_non_clinical") "items" .) }}
-    				{{ end }}
-    				{{ with $org.sliding_scale_non_clinical }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "sliding_scale_non_clinical") "items" .) }}
-    				{{ end }}
-    				{{ with $org.telehealth }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "telehealth") "items" .) }}
-    				{{ end }}
-    				{{ with $org.age_groups }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "age_groups") "items" .) }}
-    				{{ end }}
-    				{{ with $location.new_clients }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.new_clients") "items" .) }}
-    				{{ end }}
-    				{{ with $location.new_clients_detail }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.new_clients_detail") "items" .) }}
-    				{{ end }}
-    				{{ with $org.client_requirements }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "client_requirements") "items" .) }}
-    				{{ end }}
-    				{{ with $location.hours_of_operation }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.hours_of_operation") "items" .) }}
-    				{{ end }}
-    				{{ with $location.languages }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.languages") "items" .) }}
-    				{{ end }}
-    				{{ with $location.staff_gender }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.staff_gender") "items" .) }}
-    				{{ end }}
-    				{{ with $location.public_transportation }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.public_transportation") "items" .) }}
-    				{{ end }}
-    				{{ with $org.ada_compliant }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "ada_compliant") "items" .) }}
-    				{{ end }}
-    				{{ with $location.parking }}
-    					{{ partial "single-service-list" (dict "title" (partial "functions/forestryLabel" "locations.parking") "items" .) }}
-    				{{ end }} */}
+          {provider.payment_info_clinical && (
+            <SingleServiceList
+              title="PAYMENT INFO (CLINICAL)"
+              items={provider.payment_info_clinical}
+            />
+          )}
+          {provider.sliding_scale_clinical && (
+            <SingleServiceList
+              title="SLIDING SCALE (CLINICAL)"
+              items={provider.sliding_scale_clinical}
+            />
+          )}
+          {location.non_clinical_services.length > 0 && (
+            <SingleServiceList
+              title="NON-CLINICAL SERVICES"
+              items={location.non_clinical_services}
+            />
+          )}
+          {provider.payment_types_non_clinical.length > 0 && (
+            <SingleServiceList
+              title=""
+              items={provider.payment_types_non_clinical}
+            />
+          )}
+          {provider.payment_info_non_clinical && (
+            <SingleServiceList
+              title=""
+              items={provider.payment_info_non_clinical}
+            />
+          )}
+          {provider.sliding_scale_non_clinical && (
+            <SingleServiceList
+              title=""
+              items={provider.sliding_scale_non_clinical}
+            />
+          )}
+          {provider.telehealth && (
+            <SingleServiceList
+              title="TELEHEALTH SERVICE"
+              items={provider.telehealth}
+            />
+          )}
+
+          {provider.age_groups.length > 0 && (
+            <SingleServiceList title="AGE GROUPS" items={provider.age_groups} />
+          )}
+
+          {location.new_clients && (
+            <SingleServiceList
+              title="NEW CLIENTS"
+              items={location.new_clients}
+            />
+          )}
+          {location.new_clients_detail && (
+            <SingleServiceList
+              title="NEW CLIENTS DETAIL"
+              items={location.new_clients_detail}
+            />
+          )}
+          {provider.client_requirements && (
+            <SingleServiceList title="" items={provider.client_requirements} />
+          )}
+          {location.hours_of_operation.length > 0 && (
+            <SingleServiceList
+              title="HOURS OF OPERATION"
+              items={location.hours_of_operation.map((x) => x.day_hours)}
+            />
+          )}
+          {location.languages.length > 0 && (
+            <SingleServiceList title="LANGUAGES" items={location.languages} />
+          )}
+          {location.staff_gender.length > 0 && (
+            <SingleServiceList
+              title="GENDER IDENTIFICATION OF PROVIDER(S)"
+              items={location.staff_gender}
+            />
+          )}
+          {location.public_transportation.length > 0 && (
+            <SingleServiceList
+              title="PUBLIC TRANSPORTATION OPTIONS"
+              items={location.public_transportation.map(
+                (x) => x.transport_option
+              )}
+            />
+          )}
+          {provider.ada_compliant && (
+            <SingleServiceList
+              title="ADA COMPLIANT"
+              items={provider.ada_compliant}
+            />
+          )}
+          {location.parking && (
+            <SingleServiceList title="PARKING" items={location.parking} />
+          )}
         </div>
       </div>
     </div>
