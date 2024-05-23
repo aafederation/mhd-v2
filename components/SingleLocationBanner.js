0fbd26156a483806a9c7f0440d87f6e5338e51cd
rushilshakya <rushil.shakya@gmail.com>
2024-05-23 08:08:30 +0545

    feat: social media

--- components/SingleLocationBanner.js
+++ components/SingleLocationBanner.js
@@ -8,6 +8,9 @@ export const SingleLocationBanner = ({ provider, location }) => {
   let latLng = location.latLng;
   let borough = location.boroughs[0];
 
+  const trimA = (str) =>
+    str[0] === "@" ? str.substring(1).trim() : str.trim();
+
   if (provider.tags.some((x) => x.toLowerCase().includes("individual"))) {
     bgType = "bg-individual";
     colorTypeInvert = "color-individual-invert";
@@ -168,62 +171,85 @@ export const SingleLocationBanner = ({ provider, location }) => {
                 <span className="[ font_icons color-icon ] [ position-icon ]">
                   &#xe0a0;
                 </span>
-                {/* <span>
-						{provider.facebook && (
-
-							{{ $handle := trim . "@" }}
-							<span className="[ font_icons color-icon ] [ position-icon ]">
-								<a href="https://facebook.com/{{ $handle }}" target="_blank" rel="noopener noreferrer">
-									&#xe0aa;
-								</a>
-							</span>
-            )}
-						{provider.twitter && (
-
-							{{ $handle := trim . "@" }}
-							<span className="[ font_icons color-icon ] [ position-icon ]">
-								<a href="https://twitter.com/{{ $handle }}" target="_blank" rel="noopener noreferrer">
-									&#xe0ab;
-								</a>
-							</span>
-            )}
-						{provider.instagram && (
-
-							{{ $handle := trim . "@" }}
-							<span className="[ font_icons color-icon ] [ position-icon ]">
-								<a href="https://instagram.com/{{ $handle }}" target="_blank" rel="noopener noreferrer">
-									&#xe0b1;
-								</a>
-							</span>
-            )}
-						{provider.linkedin && (
-
-							{{ $handle := trim . " " }}
-							<span className="[ font_icons color-icon ] [ position-icon ]">
-								<a href="{{ $handle }}" target="_blank" rel="noopener noreferrer">
-									&#xe0b4;
-								</a>
-							</span>
-            )}
-						{ provider.youtube && (
-
-							{{ $handle := trim . " " }}
-							<span className="[ font_icons color-icon ] [ position-icon ]">
-								<a href="{{ $handle }}" target="_blank" rel="noopener noreferrer">
-									&#xe0ba;
-								</a>
-							</span>
-            )}
-						{provider.medium && (
-
-							{{ $handle := trim . "@" }}
-							<span className="[ color-icon ] [ position-icon ]">
-								<a href="https://{{ $handle }}.medium.com" target="_blank" rel="noopener noreferrer" className="svg-color">
-									<svg viewBox="0 0 1043.63 592.71" className="bu bk"><g data-name="Layer 2"><g data-name="Layer 1"><path d="M588.67 296.36c0 163.67-131.78 296.35-294.33 296.35S0 460 0 296.36 131.78 0 294.34 0s294.33 132.69 294.33 296.36M911.56 296.36c0 154.06-65.89 279-147.17 279s-147.17-124.94-147.17-279 65.88-279 147.16-279 147.17 124.9 147.17 279M1043.63 296.36c0 138-23.17 249.94-51.76 249.94s-51.75-111.91-51.75-249.94 23.17-249.94 51.75-249.94 51.76 111.9 51.76 249.94"></path></g></g></svg>
-								</a>
-							</span>
-            )}
-					</span> */}
+                <span>
+                  {provider.facebook && (
+                    <span className="[ font_icons color-icon ] [ position-icon ]">
+                      <a
+                        href={`https://facebook.com/${trimA(
+                          provider.facebook
+                        )}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        &#xe0aa;
+                      </a>
+                    </span>
+                  )}
+                  {provider.twitter && (
+                    <span className="[ font_icons color-icon ] [ position-icon ]">
+                      <a
+                        href={`https://twitter.com/${trimA(provider.twitter)}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        &#xe0ab;
+                      </a>
+                    </span>
+                  )}
+                  {provider.instagram && (
+                    <span className="[ font_icons color-icon ] [ position-icon ]">
+                      <a
+                        href={`https://instagram.com/${trimA(
+                          provider.instagram
+                        )}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        &#xe0b1;
+                      </a>
+                    </span>
+                  )}
+                  {provider.linkedin && (
+                    <span className="[ font_icons color-icon ] [ position-icon ]">
+                      <a
+                        href={provider.linkedin.trim()}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        &#xe0b4;
+                      </a>
+                    </span>
+                  )}
+                  {provider.youtube && (
+                    <span className="[ font_icons color-icon ] [ position-icon ]">
+                      <a
+                        href={provider.youtube.trim()}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        &#xe0ba;
+                      </a>
+                    </span>
+                  )}
+                  {provider.medium && (
+                    <span className="[ color-icon ] [ position-icon ]">
+                      <a
+                        href={`https://${trimA(provider.medium)}.medium.com`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="svg-color"
+                      >
+                        <svg viewBox="0 0 1043.63 592.71" className="bu bk">
+                          <g data-name="Layer 2">
+                            <g data-name="Layer 1">
+                              <path d="M588.67 296.36c0 163.67-131.78 296.35-294.33 296.35S0 460 0 296.36 131.78 0 294.34 0s294.33 132.69 294.33 296.36M911.56 296.36c0 154.06-65.89 279-147.17 279s-147.17-124.94-147.17-279 65.88-279 147.16-279 147.17 124.9 147.17 279M1043.63 296.36c0 138-23.17 249.94-51.76 249.94s-51.75-111.91-51.75-249.94 23.17-249.94 51.75-249.94 51.76 111.9 51.76 249.94"></path>
+                            </g>
+                          </g>
+                        </svg>
+                      </a>
+                    </span>
+                  )}
+                </span>
               </div>
 
               {/* {{ with .location.hours_of_operation }}
