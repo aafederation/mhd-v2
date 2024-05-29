6b6798d5edc8e8346efe1367af103125f930d7be
rushilshakya <rushil.shakya@gmail.com>
2024-05-29 14:06:58 +0545

    feat: filter showall logic

--- components/FilterData.js
+++ components/FilterData.js
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import { toggleFilter } from "../lib/utilities";
 // {{ $filter := .filter }}
 // {{ $title := .title }}
 // {{ $selectAll := print `selectAll` $filter }}
 
 export const FilterData = ({ title, allContent }) => {
+  const [filters, setFilters] = useState([]);
+  const [showAll, setShowAll] = useState("active");
   const titles = title + "s";
-  function applyFilter(el) {
-    el = el.target;
-    el.classList.toggle("active");
 
-    console.dir(el);
+  useEffect(() => {
+    console.log(filters);
+    if (filters.length === 0) setShowAll("active");
+  }, [filters]);
+
+  function applyFilter(el, content) {
+    el.stopPropagation();
+    el = el.currentTarget;
+    if (el.classList.contains("active")) {
+      el.classList.remove("active");
+      setFilters(filters.filter((x) => x !== content));
+    } else {
+      el.classList.add("active");
+      setFilters([...filters, content]);
+      setShowAll("");
+    }
   }
   return (
     <>
@@ -24,8 +39,9 @@ export const FilterData = ({ title, allContent }) => {
       <ul className="panel">
         <li>
           <button
-            id="{{ $selectAll }}"
-            // onClick="htf.showAll({{ $filter }})"
+            // id="{{ $selectAll }}"
+            className={showAll}
+            onClickCapture={(e) => applyFilter(e, title)}
           >
             All {titles}
           </button>
@@ -37,7 +53,7 @@ export const FilterData = ({ title, allContent }) => {
               // className="{{ $filter }}-button"
               // id="{{ $filter }}-{{ $termURLize }}"
               // onClick="htf.checkFilter('{{ $termURLize }}', '{{ $filter }}-')"
-              onClick={applyFilter}
+              onClickCapture={(e) => applyFilter(e, content)}
             >
               <span className="filter-item">
                 <span>{content}</span>
